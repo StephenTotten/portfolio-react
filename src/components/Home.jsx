82db3cfb2ecf74b57b910e86d431393d4aba0fbb
Crothos <s.g.totten@gmail.com>
2023-04-06 13:48:41 -0500

    tweaked work button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
     return (
@@ -12,12 +13,13 @@ const Home = () => {
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#fbfaf9]'>I'm a Full Stack Developer.</h2>
                 <p className='text-[#fbfaf9] py-4 max-w-[700px]'>After 14+ years working for a non-profit providing closed-captions for live TV, I decided to attend a Web Dev bootcamp, where I fell in love with coding. Based in Fort Worth, TX, I'm seeking entry-level positions where I can add value to a team!</p>
                 <div>
+                <Link to="work" smooth={true} duration={500}>
                     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#bbb4ab] hover:border-[#bbb4ab]'>View Work 
                     <span className='group-hover:rotate-90 duration-300'>
                     <HiArrowNarrowRight className='ml-3'/>
                     </span> 
                     </button>
-
+                </Link>
                 </div>
             </div>
         </div>
