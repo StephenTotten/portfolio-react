12a5c7bb1a5bbbc4105d08b8cd58a986b792e6df
Crothos <s.g.totten@gmail.com>
2023-04-10 12:42:09 -0500

    title and favicon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
                 </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
-                        <p>Hi. I'm Stephen, nice to meet you! Please take a look around.</p>
+                        <p>Hi, I'm Stephen. Nice to meet you! Take a look around and let's collaborate.</p>
                     </div>
                     <div>
-                        <p className='font-bold'>I graduated from Full-Stack Flex Coding Bootcamp in March of 2023 from the University of Texas at Austin. I also have a Bachelor of Arts in Radio/TV/Film from the University of North Texas, graduated 2006. Media and accessibility are important to me, and I try to incorporate those thought processes into coding.</p>
+                        <p className='font-bold'>I graduated from Full-Stack Flex Coding Bootcamp in March of 2023 from the University of Texas at Austin. I also have a Bachelor of Arts in Radio/TV/Film from the University of North Texas, graduated 2006. Media and accessibility are important to me, and I try to incorporate those thought processes into coding. After 14+ years working for a non-profit providing closed-captions for live TV, I decided to attend web dev bootcamp, where I fell in love with coding.</p>
                     </div>
                 </div>
             </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                 <p className='text-[#fbfaf9]'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#77DD77]'>Stephen Totten</h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#fbfaf9]'>I'm a Full Stack Developer.</h2>
-                <p className='text-[#fbfaf9] py-4 max-w-[700px]'>After 14+ years working for a non-profit providing closed-captions for live TV, I decided to attend a Web Dev bootcamp, where I fell in love with coding. Based in Fort Worth, TX, I'm seeking entry-level positions where I can add value to a team!</p>
+                <p className='text-[#fbfaf9] py-4 max-w-[700px]'>Currently seeking entry-level web developer positions where I can add value to a team!</p>
                 <div>
                 <Link to="work" smooth={true} duration={500}>
                     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#77DD77] hover:border-[#77DD77]'>View Work 
