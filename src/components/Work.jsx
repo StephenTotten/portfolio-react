09ae692de2551d11fcc88919668ced57711a2355
Crothos <s.g.totten@gmail.com>
2025-02-17 10:46:34 -0600

    changes to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import GitLife from '../assets/gitlife.png'
+import Aliciahb from '../assets/aliciahb.png'
+import Nplsus from '../assets/nplsus.png'
 import FlashFacts from '../assets/flashfacts.png'
 import CodeGame from '../assets/codegame.png'
 import PgaTour from '../assets/pgatour.png'
@@ -12,109 +13,118 @@ const Work = () => {
     return (
         <div name='work' className='w-full md:h-screen text-gray-300 bg-gray-800'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#77DD77]'>Personal Projects</p>
-                    <p className='py-6'>Check out some of my own projects, created with a team.</p>
-                </div>
-                {/* Container */}
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                    {/* Grid Item */}
-                    <div
-                        style={{ backgroundImage: `url(${GitLife})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2x1 font-bold text-white tracking-wider'>
-                                React/MongoDB JS Application
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://git-your-life-together.herokuapp.com/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Demo
-                                    </button>
-                                </a>
-                                <a href="https://github.com/JessicaIOrtega/Git-your-life-together-">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Code
-                                    </button>
-                                </a>
-
+                <div className='flex flex-col md:flex-row gap-8'>
+                    {/* Personal Projects Section */}
+                    <div className='w-full md:w-1/2'>
+                        <div className='pb-8'>
+                            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#77DD77]'>Personal Projects</p>
+                            <p className='py-6'>Check out some of my projects, created with a team.</p>
+                        </div>
+                        {/* Container */}
+                        <div className='grid sm:grid-cols-2 gap-4'>
+                            <div
+                                style={{ backgroundImage: `url(${Aliciahb})` }}
+                                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                                <div className='opacity-0 group-hover:opacity-100 duration-300'>
+                                    <span className='text-2x1 font-bold text-white tracking-wider'>
+                                        Therapy Website
+                                    </span>
+                                    <div className='pt-8 text-center'>
+                                        <a href="https://aliciahb.com/">
+                                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                                Visit
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                style={{ backgroundImage: `url(${FlashFacts})` }}
+                                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                                <div className='opacity-0 group-hover:opacity-100 duration-300'>
+                                    <span className='text-2x1 font-bold text-white tracking-wider'>
+                                        Flashcard App
+                                    </span>
+                                    <div className='pt-8 text-center'>
+                                        <a href="https://stephentotten.github.io/flashcard-app/">
+                                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                                Demo
+                                            </button>
+                                        </a>
+                                        <a href="https://github.com/StephenTotten/flashcard-app">
+                                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                                Code
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                style={{ backgroundImage: `url(${CodeGame})` }}
+                                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                                <div className='opacity-0 group-hover:opacity-100 duration-300'>
+                                    <span className='text-2x1 font-bold text-white tracking-wider'>
+                                        Front-End JS Application
+                                    </span>
+                                    <div className='pt-8 text-center'>
+                                        <a href="https://stephentotten.github.io/UTA-BC4-QuizGame/">
+                                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                                Demo
+                                            </button>
+                                        </a>
+                                        <a href="https://github.com/StephenTotten/UTA-BC4-QuizGame">
+                                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                                Code
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                style={{ backgroundImage: `url(${Nplsus})` }}
+                                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                                <div className='opacity-0 group-hover:opacity-100 duration-300'>
+                                    <span className='text-2x1 font-bold text-white tracking-wider'>
+                                        Business Website
+                                    </span>
+                                    <div className='pt-8 text-center'>
+                                        <a href="https://nplsus.com/">
+                                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                                Visit
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div
-                        style={{ backgroundImage: `url(${FlashFacts})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2x1 font-bold text-white tracking-wider'>
-                                Third Party API JS Application
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://stephentotten.github.io/flashcard-app/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Demo
-                                    </button>
-                                </a>
-                                <a href="https://github.com/StephenTotten/flashcard-app">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Code
-                                    </button>
-                                </a>
-
-                            </div>
+                    {/* Professional Collaboration Section */}
+                    <div className='w-full md:w-1/2'>
+                        <div className='pb-8'>
+                            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#77DD77]'>Professional Collaboration</p>
+                            <p className='py-6'>These are some of the companies I have worked with.</p>
                         </div>
-                    </div>
-                    <div
-                        style={{ backgroundImage: `url(${CodeGame})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2x1 font-bold text-white tracking-wider'>
-                                Front-End JS Application
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://stephentotten.github.io/UTA-BC4-QuizGame/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Demo
-                                    </button>
-                                </a>
-                                <a href="https://github.com/StephenTotten/UTA-BC4-QuizGame">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Code
-                                    </button>
-                                </a>
-
+                        {/* Container */}
+                        <div className='grid grid-cols-2 gap-6 text-center'>
+                            <div className='hover:scale-110 duration-500'>
+                                <img className='w-40 mx-auto' src={Presidio} alt='Presidio' />
+                            </div>
+                            <div className='hover:scale-110 duration-500'>
+                                <img className='w-40 mx-auto' src={PgaTour} alt='PGA Tour' />
+                            </div>
+                            <div className='hover:scale-110 duration-500'>
+                                <img className='w-40 mx-auto' src={Sorenson} alt='Sorenson' />
+                            </div>
+                            <div className='hover:scale-110 duration-500'>
+                                <img className='w-40 mx-auto' src={Fdc} alt='FDC' />
+                            </div>
+                            <div className='hover:scale-110 duration-500'>
+                                <img className='w-40 mx-auto' src={Soonercon} alt='Soonercon' />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='pb-8  py-6'>
-                    <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#77DD77]'>Professional Collaboration</p>
-                    <p className='py-6'>These are some of the companies I have worked with.</p>
-                </div>
-                {/* Container */}
-                <div className='w-full grid grid-rows-3 sm:grid-cols-3 gap-6 text-center'>
-                    <div className='hover:scale-110 duration-500'>
-                        <img className='w-40 mx-auto' src={Presidio} alt='node icon' />
-                    </div>
-                    <div className='hover:scale-110 duration-500'>
-                        <img className='w-40 mx-auto' src={PgaTour} alt='mongodb icon' />
-                    </div>
-                    <div className='hover:scale-110 duration-500'>
-                        <img className='w-40 mx-auto' src={Sorenson} alt='react icon' />
-                    </div>
-                    <div className='hover:scale-110 duration-500'>
-                        <img className='w-40 mx-auto' src={Fdc} alt='react icon' />
-                    </div>
-                    <div className='hover:scale-110 duration-500'>
-                        <img className='w-40 mx-auto' src={Soonercon} alt='react icon' />
-                    </div>
-                </div>
-
             </div>
         </div>
     )
