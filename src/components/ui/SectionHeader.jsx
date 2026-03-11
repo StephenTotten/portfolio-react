6bf5a724249b63d9e6c9b09fa549b601d94e8e38
Crothos <s.g.totten@gmail.com>
2026-03-11 17:00:03 -0500

    section subtitle font size update

--- src/components/ui/SectionHeader.jsx
+++ src/components/ui/SectionHeader.jsx
@@ -1,7 +1,7 @@
 const SectionHeader = ({ title, subtitle, className = '' }) => (
   <div className={className}>
     <p className='text-4xl font-bold inline border-b-4 border-[#77DD77]'>{title}</p>
-    {subtitle ? <p className='py-4'>{subtitle}</p> : null}
+    {subtitle ? <p className='py-4 text-xl'>{subtitle}</p> : null}
   </div>
 );
 
